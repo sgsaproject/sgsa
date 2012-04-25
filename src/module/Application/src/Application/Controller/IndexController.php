2b7fbffefdde2d1e5a71eb920d3fee288c499c35
Rafael <jrtadf@gmail.com>
2012-04-25 01:42:34 -0300

    closes #251 integração zf2 e doctrine mongo ODM realizada

--- src/module/Application/src/Application/Controller/IndexController.php
+++ src/module/Application/src/Application/Controller/IndexController.php
@@ -3,15 +3,25 @@
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\ActionController,
-    Zend\View\Model\ViewModel;
+    Zend\View\Model\ViewModel,
+    Application\Model\Document\User;
 
 class IndexController extends ActionController
 {
-		
-	public function indexAction()
+
+    public function indexAction()
     {
-		//$em = $this->getLocator()->get('mongo_dm');
-		//var_dump($em);
+        $dm = $this->getLocator()->get('mongo_dm');
+        //var_dump($em);
+
+        $user = new User();
+        $user->setName('Bulat S.');
+
+        // tell Doctrine 2 to save $user on the next flush()
+        $dm->persist($user);
+        $dm->flush();
+
         return new ViewModel();
     }
+
 }
