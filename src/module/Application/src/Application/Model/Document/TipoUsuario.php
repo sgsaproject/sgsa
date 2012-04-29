449daa0751fdca1e8e863c75f7c06bff342f49c0
Juliano Rodovalho <j.rodovalho.m@gmail.com>
2012-04-29 00:30:26 -0300

    add a classe de modelo, TipoUsuario.php

--- src/module/Application/src/Application/Model/Document/TipoUsuario.php
+++ src/module/Application/src/Application/Model/Document/TipoUsuario.php
@@ -5,39 +5,47 @@ namespace Application\Model\Document;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 
 /** @ODM\Document */
-class Evento {
+class TipoUsuario {
 
     /** @ODM\Id */
     private $id;
 
-    /** @ODM\Field(type="string") */
+    /** @ODM\Field (type="string") */
     private $nome;
 
     /**
-     * @return the $id
+     *
+     * @return the $id 
      */
     public function getId() {
+
         return $this->id;
     }
 
     /**
+     *
      * @return the $nome
      */
     public function getNome() {
+
         return $this->nome;
     }
 
     /**
-     * @param field_type $id
+     *
+     * @param field_type $id 
      */
     public function setId($id) {
+
         $this->id = $id;
     }
 
     /**
+     *
      * @param field_type $nome
      */
     public function setNome($nome) {
+
         $this->nome = $nome;
     }
 
