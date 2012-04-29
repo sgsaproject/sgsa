f8fbece7959b226e87ed786e3c60863d3464810b
Juliano Rodovalho <j.rodovalho.m@gmail.com>
2012-04-29 02:51:02 -0300

    Padronizando PhpDoc #ref 290

--- src/module/Application/src/Application/Model/Document/Instituicao.php
+++ src/module/Application/src/Application/Model/Document/Instituicao.php
@@ -17,7 +17,7 @@ class Instituicao {
     private $campi = array();
 
     /**
-     * Responsavel por buscar o valor da variável $id.
+     * Responsável por buscar o valor da variável $id.
      * @return int $id 
      */
     public function getId() {
@@ -26,7 +26,7 @@ class Instituicao {
 
     /**
      * 
-     * Responsavel por pegar o nome da variável "$nome".
+     * Responsável por pegar o nome da variável "$nome".
      * @return string $nome
      */
     public function getNome() {
@@ -43,7 +43,7 @@ class Instituicao {
     }
 
     /**
-     * Responsavel por setar a informação recebida por parametro($id) na variável id.
+     * Responsável por setar a informação recebida por parametro($id) na variável id.
      * @param tipo string $id 
      * @return int $id
      */
@@ -53,7 +53,7 @@ class Instituicao {
 
     /**
      * 
-     * Responsavel por setar a informação recebida por parametro($nome) na variável nome.
+     * Responsável por setar a informação recebida por parametro($nome) na variável nome.
      * @param tipo string $nome
      * @return string $nome
      */
