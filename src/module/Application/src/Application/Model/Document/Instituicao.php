4f2518dfdcca6e2bb6dd58baf2fa3ef49fa0321a
Juliano Rodovalho <j.rodovalho.m@gmail.com>
2012-04-29 02:40:02 -0300

    Padronizando PhpDoc na classe de Modelo Instituicao.php ref #290

--- src/module/Application/src/Application/Model/Document/Instituicao.php
+++ src/module/Application/src/Application/Model/Document/Instituicao.php
@@ -5,66 +5,70 @@ namespace Application\Model\Document;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 
 /** @ODM\Document */
-class Instituicao
-{
+class Instituicao {
 
     /** @ODM\Id */
     private $id;
 
     /** @ODM\Field(type="string") */
     private $nome;
-	
-	/** @ODM\ReferenceMany(targetDocument="Campus") */
-	private $campi = array();
+
+    /** @ODM\ReferenceMany(targetDocument="Campus") */
+    private $campi = array();
 
     /**
-     * @return the $id
+     * Responsavel por buscar o valor da variável $id.
+     * @return int $id 
      */
-    public function getId()
-    {
+    public function getId() {
         return $this->id;
     }
 
     /**
-     * @return the $nome
+     * 
+     * Responsavel por pegar o nome da variável "$nome".
+     * @return string $nome
      */
-    public function getNome()
-    {
+    public function getNome() {
         return $this->nome;
     }
-	
-	/**
-	 * Retorna todos os campi associados com a Instituição
-     * @return the $campi
+
+    /**
+     * 
+     * Responsavel por retornar os campi associados com a Instituição
+     * @return array campi
      */
-    public function getCampi()
-    {
+    public function getCampi() {
         return $this->campi;
     }
 
     /**
-     * @param field_type $id
+     * Responsavel por setar a informação recebida por parametro($id) na variável id.
+     * @param tipo string $id 
+     * @return int $id
      */
-    public function setId($id)
-    {
+    public function setId($id) {
         $this->id = $id;
     }
 
     /**
-     * @param field_type $nome
+     * 
+     * Responsavel por setar a informação recebida por parametro($nome) na variável nome.
+     * @param tipo string $nome
+     * @return string $nome
      */
-    public function setNome($nome)
-    {
+    public function setNome($nome) {
         $this->nome = $nome;
     }
-	
-	/**
-	 * Seta os campi associados com a Instituição
-     * @param field_type $campi
+
+    /**
+     * 
+     * Responsavel por setar as informações recebidas por parametro($campi) no array de campi associados com a Instituição.
+     * @param tipo array $campi
+     * @return array $campi
      */
-    public function setCampi($campi)
-    {
+    public function setCampi($campi) {
         $this->campi = $campi;
     }
-	
+
 }
