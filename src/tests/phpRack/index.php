c2e3d09047721001a182b925e321d4b72d05ae04
Rafael <jrtadf@gmail.com>
2012-04-29 00:09:59 -0300

    phpRack path fixes

--- src/tests/phpRack/index.php
+++ src/tests/phpRack/index.php
@@ -1,7 +1,7 @@
 <?php
 // this param is mandatory, others are optional
 $phpRackConfig = array(
-    'dir' => 'rack-tests',
+    'dir' =>  __DIR__. DIRECTORY_SEPARATOR. 'rack-tests',
 );
 // absolute path to the bootstrap script on your server
-include 'phpRack/bootstrap.php';
+include(__DIR__. DIRECTORY_SEPARATOR.'phpRack'. DIRECTORY_SEPARATOR.'bootstrap.php');
