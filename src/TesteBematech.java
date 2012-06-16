ab194efd26574c3b93a0fca365707539953fd1c9
Thiago Krug <thiagockrug@gmail.com>
2012-06-16 00:46:00 -0300

    troca para os arquivos fonte certos

--- src/TesteBematech.java
+++ src/TesteBematech.java
@@ -3,19 +3,23 @@
  * and open the template in the editor.
  */
 
-
 import com.sun.jna.Native;
+import javax.swing.JOptionPane;
 
 class TesteBematech {
-   
+
     public static void main(String[] args) {
-//        System.loadLibrary("mp2032");
-        Mp2032 lib = (Mp2032) Native.loadLibrary("mp2032", Mp2032.class);
-        System.out.println(lib.IniciaPorta("COM3"));
-        System.out.println(lib.ConfiguraModeloImpressora(5));
-        System.out.println(lib.BematechTX("teste\n"));
-        System.out.println(lib.BematechTX("teste\n"));
-        System.out.println(lib.AcionaGuilhotina(1));
-        System.out.println(lib.FechaPorta());
+        try {
+            Mp2032 lib = (Mp2032) Native.loadLibrary("mp2032", Mp2032.class);
+            JOptionPane.showMessageDialog(null, lib.IniciaPorta("COM3"));
+            JOptionPane.showMessageDialog(null, lib.ConfiguraModeloImpressora(7));
+            JOptionPane.showMessageDialog(null, lib.BematechTX("teste\n"));
+            JOptionPane.showMessageDialog(null, lib.BematechTX("teste\n"));
+            JOptionPane.showMessageDialog(null, lib.AcionaGuilhotina(1));
+            JOptionPane.showMessageDialog(null, lib.FechaPorta());
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, e.getStackTrace());
+        }
     }
 }
