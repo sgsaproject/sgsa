69a7199747ccafd7141eb7052666fe6774396c6d
Rafael <jrtadf@gmail.com>
2012-07-06 02:08:32 -0300

    fixtures

---
+++ data/fixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Inserindo valores padrões
+ */
+echo 'Inserindo dados padrões' . BREAK_LINE;
+
+
+
+
+if (APPLICATION_ENV == 'development' || APPLICATION_ENV == 'staging' || APPLICATION_ENV == 'production'):
+
+endif;
+
+if (APPLICATION_ENV == 'testing'):
+    
+endif;
