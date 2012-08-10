9372625ca59f87a5c130b16e3ac4419f7a5500a4
Rafael <jrtadf@gmail.com>
2012-08-10 05:03:02 -0300

    teste da classe utils

--- library/Sistema/Model/Utils.php
+++ library/Sistema/Model/Utils.php
@@ -8,18 +8,16 @@
 trait Sistema_Model_Utils {
 
     public function setAttributes(array $data = null) {
-        if (empty($data) == false) {
-            foreach ($data as $key => $value) {
-                $array = explode('_', $key);
-                array_map('ucfirst', $array);
-                $key = implode('', $array);
-                $method = 'set' . ucfirst($key);
-                if (method_exists($this, $method)) {
-                    $this->{$method}($value);
-                    return;
-                }
-                throw new BadMethodCallException('Call to undefined method');
+        foreach ($data as $key => $value) {
+            $array = explode('_', $key);
+            array_map('ucfirst', $array);
+            $key = implode('', $array);
+            $method = 'set' . $key;
+            if (method_exists($this, $method)) {
+                $this->{$method}($value);
+                return;
             }
+            throw new BadMethodCallException('Call to undefined method');
         }
     }
 
