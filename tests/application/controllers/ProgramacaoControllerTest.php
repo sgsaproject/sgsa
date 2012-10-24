b3351f7224a62bbd65d8bd00361e1845825602cc
Thiago Krug <thiagockrug@gmail.com>
2012-10-24 21:27:55 -0200

    descomentario dos testes

--- tests/application/controllers/ProgramacaoControllerTest.php
+++ tests/application/controllers/ProgramacaoControllerTest.php
@@ -1,12 +1,12 @@
 <?php
 
 class ProgramacaoControllerTest extends Zend_Test_PHPUnit_ControllerTestCase {
-/*
+
     public function setUp() {
         $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
         parent::setUp();
     }
-    
+
     public function testNenhumaPalestra() {
         $palestraDAO = new Application_Model_DbTable_Palestra();
         $palestra = $palestraDAO->createRow();
@@ -19,17 +19,17 @@ class ProgramacaoControllerTest extends Zend_Test_PHPUnit_ControllerTestCase {
         $palestra->setHoraFim("20/08/2012 20:13:03");
         $palestra->setSala(101);
         $palestra->save();
-        
+
         $palestras = $palestraDAO->fetchAll();
-        
-        /*@var $palestra Application_Model_Palestra*/
-        /*foreach ($palestras as $palestra) {
+
+        /* @var $palestra Application_Model_Palestra */
+        foreach ($palestras as $palestra) {
             $deleted = $palestra->delete();
             if ($deleted <= 0) {
                 $this->fail("Palestra " . $palestra->getNomePalestra() . " não foi deletada");
             }
         }
-        
+
         $params = array('action' => 'index', 'controller' => 'programacao', 'module' => 'default');
         $urlParams = $this->urlizeOptions($params);
         $url = $this->url($urlParams);
@@ -42,7 +42,7 @@ class ProgramacaoControllerTest extends Zend_Test_PHPUnit_ControllerTestCase {
         $this->assertAction($urlParams['action']);
         $this->assertQueryCount("tr", 1);
     }
-    
+
     public function testVariasPalestras() {
         $palestraDAO = new Application_Model_DbTable_Palestra();
         $palestra = $palestraDAO->createRow();
@@ -55,7 +55,7 @@ class ProgramacaoControllerTest extends Zend_Test_PHPUnit_ControllerTestCase {
         $palestra->setHoraFim("02/08/2012 10:31:03");
         $palestra->setSala(101);
         $palestra->save();
-        
+
         $palestra = $palestraDAO->createRow();
         $palestra->setNomePalestra("TDD");
         $palestra->setNomePalestrante("Kent Beck");
@@ -66,7 +66,7 @@ class ProgramacaoControllerTest extends Zend_Test_PHPUnit_ControllerTestCase {
         $palestra->setHoraFim("02/08/2012 20:13:03");
         $palestra->setSala(101);
         $palestra->save();
-        
+
         $palestra = $palestraDAO->createRow();
         $palestra->setNomePalestra("Engenharia de Software");
         $palestra->setNomePalestrante("Cléo");
@@ -77,7 +77,7 @@ class ProgramacaoControllerTest extends Zend_Test_PHPUnit_ControllerTestCase {
         $palestra->setHoraFim("20/08/2012 20:13:03");
         $palestra->setSala(101);
         $palestra->save();
-        
+
         $palestra = $palestraDAO->createRow();
         $palestra->setNomePalestra("Engenharia de Software");
         $palestra->setNomePalestrante("Cléo");
@@ -88,9 +88,9 @@ class ProgramacaoControllerTest extends Zend_Test_PHPUnit_ControllerTestCase {
         $palestra->setHoraFim("20/08/2012 20:13:03");
         $palestra->setSala(101);
         $palestra->save();
-        
+
         $count = $palestraDAO->fetchAll()->count();
-        
+
         $params = array('action' => 'index', 'controller' => 'programacao', 'module' => 'default');
         $urlParams = $this->urlizeOptions($params);
         $url = $this->url($urlParams);
@@ -103,5 +103,5 @@ class ProgramacaoControllerTest extends Zend_Test_PHPUnit_ControllerTestCase {
         $this->assertAction($urlParams['action']);
         $this->assertQueryCount("tr", $count + 1);
     }
-    */
+
 }
