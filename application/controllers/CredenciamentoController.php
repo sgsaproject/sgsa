85a4f6470b572ef05ded39f6641c0b84d024802b
Rafael <jrtadf@gmail.com>
2013-08-17 17:45:13 -0300

    Iniciando credenciamento

--- application/controllers/CredenciamentoController.php
+++ application/controllers/CredenciamentoController.php
@@ -7,10 +7,24 @@ class CredenciamentoController extends Zend_Controller_Action {
     }
 
     public function indexAction() {
+
+    }
+    
+    public function credenciarAction() {
+        $usuarioDbTable = new Application_Model_DbTable_Usuario();
+        $usuario = $usuarioDbTable->find($this->getRequest()->getParam('id_usuario'))->current();
+        
         $fPagamentoDAO = new Application_Model_DbTable_FormaPagamento();
         $formasPagamento = $fPagamentoDAO->getFormasPagamento();
-        //$this->view->usuario = $usuario;
+        
+        
+        if($this->getRequest()->isPost()){
+            
+        }
+        
+        
         $this->view->formasPagamento = $formasPagamento;
+        $this->view->usuario = $usuario;
     }
 
     public function pagarAction() {
